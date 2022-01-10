a3f9eef271f1635b007d139341caf72cf9bceedf
sambarboza <sambarbosaa@gmail.com>
2022-01-09 20:06:50 -0500

    only querying on new blocks

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use crate::models::{Response, ReverseSimulationResponse};
 use actix_web::rt::{spawn, time};
 use actix_web::{App, HttpServer};
 use anyhow::Result;
+use cosmwasm_std::Uint128;
 use serde_json::{json, to_string};
+use std::ops::Div;
 use std::time::Duration;
 use terra_rust_api::Terra;
 
@@ -13,16 +15,17 @@ async fn main() -> std::io::Result<()> {
     let terra = Terra::lcd_client_no_tx("http://143.244.190.1:3060", "localterra");
     spawn(async move {
         let mut interval = time::interval(Duration::from_secs(3));
-        // let mut last_height = 0;
+        let mut last_height = 0;
         loop {
             let last_block = &terra.tendermint().blocks().await;
             match last_block {
-                Ok(block) => {
-                    //Get Height, if diff than last height, query ReverseSimulation and store it. :)
-                    println!("Last Block {:?}", block);
-                    let block_time = block.block.header.time.timestamp().unsigned_abs();
-                    query_reverse_simulation(&terra, block_time).await;
-                    println!("Block fetched.")
+                Ok(block_result) => {
+                    let block_height = block_result.block.header.height;
+                    if block_height > last_height {
+                        last_height = block_height.clone();
+                        let block_time = block_result.block.header.time.timestamp().unsigned_abs();
+                        query_reverse_simulation(&terra, block_time).await;
+                    }
                 }
                 Err(_) => {
                     println!("Couldn't fetch latest block")
@@ -55,16 +58,19 @@ async fn query_reverse_simulation(terra: &Terra, block_time: u64) {
             "block_time": block_time
         }
     });
+
     let json_query = to_string(&query_msg).unwrap();
-    println!("Execute Query: {:?}", json_query.as_str());
     let result: Result<Response<ReverseSimulationResponse>> =
         terra.wasm().query(pool_addr, json_query.as_str()).await;
+
     match result {
         Ok(response) => {
-            println!("Query Success: {:?}", response);
+            println!("Query Success.");
         }
         Err(e) => {
             println!("Query Error: {:?}", e);
         }
     }
 }
+
+async fn save(response: ReverseSimulationResponse) {}
