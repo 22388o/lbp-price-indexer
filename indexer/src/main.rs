6f2a3eb3cfeec04906e66f8093824ae594033d61
Samuel Barbosa <sambarbosaa@gmail.com>
2022-01-13 15:38:37 -0500

    checking if lbp started before querying price.

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,34 +1,35 @@
-use crate::models::{Response, ReverseSimulationResponse};
+use crate::models::{PairInfo, Response, ReverseSimulationResponse};
 use actix_web::rt::{spawn, time};
 use actix_web::{App, HttpServer};
-use anyhow::Result;
+use anyhow::Result as AnyhowResult;
 use mysql::prelude::Queryable;
 use mysql::PooledConn;
 use mysql::{params, OptsBuilder, Pool};
 use serde_json::{json, to_string};
+use std::env::var;
 use std::time::Duration;
 use terra_rust_api::Terra;
-use std::env::{var};
 
 pub mod models;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let pass = match var("DBPASS") {
-        Ok(pass) => {
-            Some(pass)
-        }
-        Err(_) => {
-            None
-        }
+        Ok(pass) => Some(pass),
+        Err(_) => None,
     };
     let mysql_pool = Pool::new(
         OptsBuilder::new()
+            .tcp_connect_timeout(Some(Duration::from_secs(10)))
             .user(Some(var("DBUSER").unwrap_or("root".to_string())))
+            .ip_or_hostname(Some(var("DBHOST").unwrap_or("127.0.0.1".to_string())))
             .pass(pass)
-            .db_name(Some(var("DBNAME").unwrap_or("indexer".to_string()).to_string())),
+            .db_name(Some(
+                var("DBNAME").unwrap_or("indexer".to_string()).to_string(),
+            )),
     )
     .unwrap();
+    println!("hello");
 
     let lcd_url = var("LCDURL").unwrap();
     let chain_id = var("CHAINID").unwrap();
@@ -64,10 +65,25 @@ async fn main() -> std::io::Result<()> {
         .await
 }
 
+async fn query_pair_info(terra: &Terra, pool: String) -> anyhow::Result<Response<PairInfo>> {
+    let result: AnyhowResult<Response<PairInfo>> = terra
+        .wasm()
+        .query(
+            pool.as_str(),
+            to_string(&json!({"pair":{}})).unwrap().as_str(),
+        )
+        .await;
+    result
+}
+
 async fn query_reverse_simulation(terra: &Terra, block_time: u64, mut mysql_conn: PooledConn) {
     let pool_addr = var("POOL").unwrap();
     let token_addr = var("TOKEN").unwrap();
-    let query_msg = json!({
+
+    let pair_info_query_result = query_pair_info(terra, pool_addr.clone()).await.unwrap();
+    println!("pair_info: {:?}", pair_info_query_result);
+
+    let reverse_simulation_query = json!({
         "reverse_simulation" : {
             "ask_asset": {
                 "info": {
@@ -81,19 +97,22 @@ async fn query_reverse_simulation(terra: &Terra, block_time: u64, mut mysql_conn
         }
     });
 
-    let json_query = to_string(&query_msg).unwrap();
-    let result: Result<Response<ReverseSimulationResponse>> =
-        terra.wasm().query(pool_addr.as_str(), json_query.as_str()).await;
+    if pair_info_query_result.result.start_time >= block_time {
+        let json_query = to_string(&reverse_simulation_query).unwrap();
+        let result: AnyhowResult<Response<ReverseSimulationResponse>> = terra
+            .wasm()
+            .query(pool_addr.as_str(), json_query.as_str())
+            .await;
 
-    match result {
-        Ok(response) => {
-            println!("Query Success.");
-            let result = &response.result;
-            let mysql_result = mysql_conn.exec_drop(
-                r"INSERT INTO reverse_simulation 
+        match result {
+            Ok(response) => {
+                println!("Query Success.");
+                let result = &response.result;
+                let mysql_result = mysql_conn.exec_drop(
+                    r"INSERT INTO reverse_simulation 
                 (height, offer_amount, spread_amount, commission_amount, ask_weight, offer_weight, block_time) 
                 VALUES (:height, :offer_amount, :spread_amount, :commission_amount, :ask_weight, :offer_weight, :block_time)",
-                params! {
+                    params! {
                     "height" => &response.height,
                     "offer_amount" => &result.offer_amount.to_string(),
                     "spread_amount" => &result.spread_amount.to_string(),
@@ -101,12 +120,13 @@ async fn query_reverse_simulation(terra: &Terra, block_time: u64, mut mysql_conn
                     "ask_weight" => &result.ask_weight,
                     "offer_weight" => &result.offer_weight,
                     "block_time" => block_time.clone()
-                }
-            );
-            println!("Insert result: {:?}", mysql_result);
-        }
-        Err(e) => {
-            println!("Query Error: {:?}", e);
+                },
+                );
+                println!("Insert result: {:?}", mysql_result);
+            }
+            Err(e) => {
+                println!("Query Error: {:?}", e);
+            }
         }
     }
 }
--- indexer/src/models.rs
+++ indexer/src/models.rs
@@ -15,3 +15,27 @@ pub struct ReverseSimulationResponse {
     pub ask_weight: String,
     pub offer_weight: String,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum AssetInfo {
+    Token { contract_addr: String },
+    NativeToken { denom: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct WeightedAssetInfo {
+    pub info: AssetInfo,
+    pub start_weight: String,
+    pub end_weight: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct PairInfo {
+    pub asset_infos: [WeightedAssetInfo; 2],
+    pub contract_addr: String,
+    pub liquidity_token: String,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub description: Option<String>,
+}
